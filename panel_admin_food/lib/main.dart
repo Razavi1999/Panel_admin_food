c7e8fde1e5a9456fc0114fa9758958707c6a7cc2
sinaziaee <sinaziaee99@gmail.com>
2020-12-16 23:17:22 +0330

    changing

---
+++ panel_admin_food/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'screens/food_details_screen.dart';
+import 'screens/history_screen.dart';
+import 'screens/login_screen.dart';
+import 'constants.dart';
+import 'screens/new_food_screen.dart';
+import 'screens/registeration_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/food_screen.dart';
+import 'screens/request_screen.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        accentColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      initialRoute: LoginScreen.id,
+      routes: {
+        RegisterationScreen.id: (context) => RegisterationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        HomeScreen.id :(context) => HomeScreen(),
+        OrderPage.id:(context) => OrderPage(),
+        NewFoodScreen.id: (context) => NewFoodScreen(),
+        HistoryScreen.id: (context) => HistoryScreen(),
+        FoodDetailsScreen.id: (context) => FoodDetailsScreen(),
+        RequestScreen.id: (context) => RequestScreen(),
+      },
+    );
+  }
+}
+
